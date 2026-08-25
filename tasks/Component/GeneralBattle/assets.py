e0aaf5de71f237f804c4e1faf208568754e74758
573780986 <573780986@qq.com>
2026-08-25 22:20:27 +0800

    fix(GeneralBattle): C_REWARD_2 缩窄 ROI 避开左侧悬浮窗

--- tasks/Component/GeneralBattle/assets.py
+++ tasks/Component/GeneralBattle/assets.py
@@ -20,7 +20,7 @@ class GeneralBattleAssets:
 	# description 
 	C_REWARD_1 = RuleClick(roi_front=(606,603,325,87), roi_back=(606,603,325,87), name="reward_1")
 	# description 
-	C_REWARD_2 = RuleClick(roi_front=(25,134,224,472), roi_back=(25,134,224,472), name="reward_2")
+	C_REWARD_2 = RuleClick(roi_front=(50,134,199,472), roi_back=(50,134,199,472), name="reward_2")
 	# description 
 	C_REWARD_3 = RuleClick(roi_front=(1092,156,168,437), roi_back=(1092,156,168,437), name="reward_3")
 
